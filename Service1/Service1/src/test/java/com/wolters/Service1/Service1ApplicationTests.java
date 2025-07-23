89b4c089b75eb6cfa857c118b8182b6895dcd55b
Shantanu Bijwar <108397883+shantu07411@users.noreply.github.com>
2025-07-24 01:45:31 +0530

    Update Service1ApplicationTests.java

--- Service1/Service1/src/test/java/com/wolters/Service1/Service1ApplicationTests.java
+++ Service1/Service1/src/test/java/com/wolters/Service1/Service1ApplicationTests.java
@@ -3,7 +3,7 @@ package com.wolters.Service1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Service1Application.class)
 class Service1ApplicationTests {
 
 	@Test
